4db1339e4c26f21396ab6f85c98c7a0c0b9e214e
Diego Torres <imt.dtorres@gmail.com>
2025-05-08 17:27:22 -0600

    Arduino code added
    - Wrote an Arduino script to control the LED otuput.

---
+++ 3rd party/LED_Control/LED_Control.ino
@@ -0,0 +1,45 @@
+const int Red_pin = 8;
+const int Green_pin = 9;
+const int Blue_pin = 10;
+
+void setup() {
+  Serial.begin(115200);  // setup Serial library at 115200 bps
+  while (!Serial) {
+    Serial.println("Starting...");
+  }
+  pinMode(Red_pin,OUTPUT);
+  pinMode(Green_pin,OUTPUT);
+  pinMode(Blue_pin,OUTPUT);
+
+  digitalWrite(Red_pin, LOW);
+  digitalWrite(Green_pin, LOW);
+  digitalWrite(Blue_pin, LOW);
+  delay(20);
+  Serial.println("LED controller");
+}
+
+void loop() {
+  if(Serial.available() >= 4){  // Data at port
+    // Read data at port
+    char Red = Serial.read();
+    char Green = Serial.read();
+    char Blue = Serial.read();
+
+    // Set pins based on received values
+    digitalWrite(Red_pin, (Red == '1') ? HIGH : LOW);
+    digitalWrite(Green_pin, (Green == '1') ? HIGH : LOW);
+    digitalWrite(Blue_pin, (Blue == '1') ? HIGH : LOW);
+
+    while(Serial.available() > 0){
+      Serial.read(); // Flush serial port
+    }
+    /* 
+    Serial.print("Set pins: ");
+    Serial.print(Red_pin); Serial.print("="); Serial.print(Red);
+    Serial.print(", ");
+    Serial.print(Green_pin); Serial.print("="); Serial.print(Green);
+    Serial.print(", ");
+    Serial.print(Blue_pin); Serial.print("="); Serial.println(Blue);
+    */
+  } // End of Serial.available()
+} // End of loop()
